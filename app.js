a65aab71167e75ee4e302d6c4f43191b7082bff2
bochen-chen <code.bochen@gmail.com>
2018-07-24 18:38:34 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const routes = require('./routes/index');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+  res.end();
+});
+
+// error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.listen(PORT, () => {
+  console.log(`service running on port: ${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/echo', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.json({
+    response_type: 'ephemeral',
+    text: `${req.body.text}`
+  });
+});
+
+module.exports = router;
