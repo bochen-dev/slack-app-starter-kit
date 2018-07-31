b83bf593a1ea8b92ecbefae139fc0f8e9b64e04d
bochen-chen <code.bochen@gmail.com>
2018-07-31 11:34:38 +0800

    ignore config

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  slack_signing_secret: '5e393315814f75e991a44558b6ebd486',
-  verification_token: 'yQ84YYEAJIj7JB0nUmFYnNta'
-};
---
+++ config/index_sample.js
@@ -0,0 +1,4 @@
+module.exports = {
+  slack_signing_secret: 'YOUR_SLACK_SIGNING_SECRET',
+  verification_token: 'YOUR_VERIFICATION_TOKEN'
+};
